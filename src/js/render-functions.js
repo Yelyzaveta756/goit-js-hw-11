8f2e14e3e73b267cde2d1e6e69aef4189a064c0d
Yelyzaveta756 <146189076+Yelyzaveta756@users.noreply.github.com>
2024-03-03 15:44:06 +0200

    final

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,46 +1,37 @@
-export function renderData(data) {
-    // console.log(data);
-    const img = data.hits;
-    const arrayOfImg = img.map(
-        ({
-          webformatURL,
-          largeImageURL,
-          tags,
-          likes,
-          views,
-          comments,
-          downloads,
-        }) => {
-          return `<li class="list-link">
-                          
-                              <a href="${largeImageURL}">
-                              <img class="gallery-links" src="${webformatURL}" alt="${tags}"></a>
-                          
-                              <div class="parameters">
-                                  <ul class="parameters-list">
-                                      <li class="parameters-list-item">likes:</li>
-                                      <li class="parameters-list-item">${likes}</li>
-                                  </ul>
-                                  <ul class="parameters-list">
-                                      <li class="parameters-list-item">views:</li>
-                                      <li class="parameters-list-item">${views}</li>
-                                  </ul>
-                                  <ul class="parameters-list">
-                                      <li class="parameters-list-item">comments:</li>
-                                      <li class="parameters-list-item">${comments}</li>
-                                  </ul>
-                                  <ul class="parameters-list">
-                                      <li class="parameters-list-item">downloads:</li>
-                                      <li class="parameters-list-item">${downloads}</li>
-                                  </ul>
-                          </div>
-                          </li>`;
-        }
-      )
-      .join('');
-  
-    gallery.innerHTML = arrayOfImg;
-    lightbox.refresh(
-      'https://github.com/andreknieriem/simplelightbox#public-methods'
-    );
+
+export function createMarkup(arr) {
+
+  return arr.hits
+    .map(({largeImageURL, 
+      webformatURL, 
+      tags, 
+      likes, 
+      views, 
+      comments, 
+      downloads}) => 
+      `<div class="gallery-item>
+      <a class="gallery-link" href="${largeImageURL}">
+          <img class="gallery-image" src="${webformatURL}" alt="${tags}" width="360px" height="260px"/>
+      </a>
+      <div class="gallery-info">
+          <p class="gallery-info-par">
+              <span class="gallery-info-span">Likes: <span class="tag-span">${likes}</span>
+              </span>    
+          </p>
+          <p class="gallery-info-par">
+              <span class="gallery-info-span">Views: <span class="tag-span">${views}</span>
+              </span>    
+          </p>
+          <p class="gallery-info-par">
+              <span class="gallery-info-span">Comments: <span class="tag-span">${comments}</span>
+              </span>    
+          </p>
+          <p class="gallery-info-par">
+              <span class="gallery-info-span">Downloads: <span class="tag-span">${downloads}</span>
+              </span>    
+          </p>
+      </div>
+  </div>`
+    )
+    .join('');
 }
--- src/main.js
+++ src/main.js
@@ -1,9 +1,50 @@
-
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
-import searchImages from './js/pixabay-api';
-import createMarcupGallery from './js/render-functions';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import {createMarkup} from "./js/render-functions.js"
+import {searchImages} from "./js/pixabay-api.js"
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+const searchForm = document.querySelector('.form');
+const gallery = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
+
+searchForm.addEventListener('submit', handleSearch);
+loader.style.display = 'none';
+
+function handleSearch(event) {
+  event.preventDefault();
+
+  loader.style.display = 'inline-block';
+
+  gallery.innerHTML = '';
+  const form = event.currentTarget;
+  const QUERY = form.elements.query.value.trim();
+
+  if (QUERY === '') {
+    iziToast.show({
+      title: 'Error',
+      color: 'yellow',
+      message: 'Please search for something',
+    });
+    loader.style.display = 'none';
+    return;
+  }
+
+  searchImages(QUERY)
+  .then(arr => {
+    gallery.innerHTML = createMarkup(arr);
+    lightbox.refresh();
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+    form.reset();
+}
