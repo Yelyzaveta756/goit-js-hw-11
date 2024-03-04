c11b183431e7c64745b7a7152c6812d171cbe566
Yelyzaveta756 <146189076+Yelyzaveta756@users.noreply.github.com>
2024-03-05 00:50:25 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import {createMarkup} from "./js/render-functions.js"
 import {searchImages} from "./js/pixabay-api.js"
 
-const lightbox = new SimpleLightbox('.gallery a ', {
+const lightbox = new SimpleLightbox('.gallery div ', {
   captionsData: 'alt',
   captionDelay: 250,
 });
